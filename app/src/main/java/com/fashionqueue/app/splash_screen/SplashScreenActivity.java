5cb5c71cf57bf4388efa9214eb67cd7361ee46db
ashokCB <ashok.reddy@codebakerz.in>
2019-01-28 20:49:42 +0530

    My First Commit

---
+++ app/src/main/java/com/fashionqueue/app/splash_screen/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.fashionqueue.app.splash_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fashionqueue.app.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
