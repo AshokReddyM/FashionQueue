bbf06277d0a1ed19579092577480f5b37061a411
Ashok Reddy M <aaashokreddy@gmail.com>
2019-02-01 00:35:47 +0530

    update

---
+++ app/src/main/java/com/fashionqueue/app/gender/GenderActivity.java
@@ -0,0 +1,15 @@
+package com.fashionqueue.app.gender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fashionqueue.app.R;
+
+public class GenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gender);
+    }
+}
